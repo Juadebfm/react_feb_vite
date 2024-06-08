cc07495c51cb4213715eae9182d0330ebbacaa92
Julius Adebowale Gabriel <juadebgabriel@gmail.com>
2024-06-08 13:36:23 +0100

    Cleaned up folder

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import "./index.css";
+function App() {
+  return (
+    <>
+      <header>
+        <nav>
+          <div>Logo</div>
+          <ul>
+            <li>
+              <a href="">Nav Itens</a>
+            </li>
+            <li>
+              <a href="">Nav Itens</a>
+            </li>
+            <li>
+              <a href="">Nav Itens</a>
+            </li>
+            <li>
+              <a href="">Nav Itens</a>
+            </li>
+            <li>
+              <a href="">Nav Itens</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main></main>
+      <footer></footer>
+    </>
+  );
+}
+
+export default App;
