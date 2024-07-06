67a590820050c40ce0cf5e95f564544ed77ac0c8
Julius Adebowale Gabriel <juadebgabriel@gmail.com>
2024-07-06 13:42:38 +0100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import StartupComponentOne from "./components/StartupComponentOne";
-import About from "./pages/About";
+// import About from "./pages/About";
 import Features from "./pages/Features";
-import UseState from "./components/UseState";
+// import UseState from "./components/UseState";
 import { WeatherProvider } from "./context/WeatherContext";
 import WeatherDetails from "./components/WeatherDetails";
+import UseEffect from "./components/UseEffect";
 // import Navbar from "./components/Navbar";
+// import UseEffect from "./components/UseEffect";
 
 const App = () => {
   return (
@@ -17,9 +19,9 @@ const App = () => {
           <Route path="/" element={<StartupComponentOne />} />
           <Route path="/shop" element={<WeatherDetails />} />
 
-          <Route path="/about" element={<About />} />
+          <Route path="/about" element={<UseEffect />} />
           <Route path="/features" element={<Features />} />
-          <Route path="/usestate" element={<UseState />} />
+          {/* <Route path="/usestate" element={<UseState />} /> */}
         </Routes>
       </WeatherProvider>
     </BrowserRouter>
--- src/components/UseState.jsx
+++ src/components/UseState.jsx
@@ -1,28 +1,24 @@
 import React, { useState } from "react";
 
 const UseState = () => {
-  const [count, setCount] = useState(0);
+  const [isToggled, setIsToggled] = useState(false);
 
-  const increment = () => {
-    setCount(count - 1);
+  const handleClick = () => {
+    setIsToggled(!isToggled);
   };
 
+  const buttonClass = isToggled
+    ? "bg-blue-500 text-white py-2 px-4 rounded-md"
+    : "bg-gray-500 text-white py-2 px-4 rounded-md";
+
+  const buttonText = isToggled ? "Blue Bg" : "Gray Bg";
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-slate-500 text-white">
-      <small>
-        <a href="/">Go Back</a>
-      </small>
-      <h1 className="font-bold text-3xl text-center">Count: {count}</h1>
-      <button
-        className="bg-blue-500 text-white text-sm rounded-md px-8 py-2 cursor-pointer hover:shadow-xl mt-8"
-        onClick={increment}
-      >
-        Increase Count Value
-      </button>
-    </div>
+    <button className={buttonClass} onClick={handleClick}>
+      {buttonText}
+    </button>
   );
 };
 
 export default UseState;
 
-// import the useState hook from react
+fetch().then().then().then();
