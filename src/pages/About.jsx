07679896b5cfb17982ee8efe5b97b5a5f5223438
Julius Adebowale Gabriel <juadebgabriel@gmail.com>
2024-07-12 21:55:16 +0100

    Start new branch afresh

--- src/pages/About.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const About = () => {
-  return <div className="h-screen bg-black text-white">About Page</div>;
-};
-
-export default About;
--- src/pages/Features.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Features = () => {
-  return <div className="h-screen bg-black text-white">Features</div>;
-};
-
-export default Features;
